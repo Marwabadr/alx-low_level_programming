bae9c0f76ca589e6ccfcc53bf8e91411f4b45d0a
Marwabadr <elbadrmarwa2002@gmail.com>
2023-08-10 08:40:07 -0700

    a quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - Entry
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and taht piece of art is useful\"- Dora Korpar, @015-10-19\n", 59);
+	return (1);
+}
