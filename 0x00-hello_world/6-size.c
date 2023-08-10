55ef6826b5fd3bd0a4f87116be59ba0d46c98995
Marwabadr <elbadrmarwa2002@gmail.com>
2023-08-10 08:19:46 -0700

    size of variable

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("the size of char : %i\n", sizeof(char));
+	printf("the size of int : %i\n", sizeof(int));
+	printf("the size of long int : %i\n", sizeof(long int));
+	printf("the size of long long int : %i\n", sizeof(long long int));
+	printf("the size of float : %i\n", sizeof(float));
+	return (0);
+}
