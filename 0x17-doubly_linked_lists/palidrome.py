78dc98d977471dc22fcb051b7b284703acbfc0a5
Marwabadr <elbadrmarwa2002@gmail.com>
2023-12-07 12:50:05 -0800

    key generator

---
+++ 0x17-doubly_linked_lists/palidrome.py
@@ -0,0 +1,13 @@
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+max_palindrome = 0
+
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        product = i * j
+        if is_palindrome(product) and product > max_palindrome:
+            max_palindrome = product
+
+with open('102-result', 'w') as file:
+    file.write(str(max_palindrome))
